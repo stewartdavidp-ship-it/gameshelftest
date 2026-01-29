6f54b0e066051e3db050612632325d14025bbd91
David Stewart <stewartdavidp@gmail.com>
2026-01-29 16:44:29 -0500

    Deploy Game Shelf v1.3.37 (29 files)

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  * Update this on EVERY release to force PWA update.
  */
 
-const CACHE_VERSION = 'v1.3.36';
+const CACHE_VERSION = 'v1.3.37';
 const CACHE_NAME = `gameshelf-pwa-${CACHE_VERSION}`;
 
 // Install - skip waiting immediately to activate new SW
